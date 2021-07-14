c5f548f0fa8cd551af27b4c18e4a327918e5204a
Kevin Morris <kevr@0cost.org>
2021-07-14 01:15:33 -0700

    set environment paths relative to $(pwd)
    This is the first step on our goal of no user installations.
    
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@
 set -eou pipefail
 
 /usr/bin/pip3 install -I --user -r requirements.txt .
-cp -v examples/mdpreviewrc $HOME/.mdpreviewrc
+
+sed -i "s|SCRIPT_PATH|$(pwd)/bin|" \
+    $HOME/.config/systemd/user/mdpreviewd.service
+
+cp -vf examples/mdpreviewrc $HOME/.mdpreviewrc
+sed -ri "s|^PYTHONPATH=.*$|PYTHONPATH=\"$(pwd)\"|" $HOME/.mdpreviewrc
+sed -ri "s|^PATH=.*$|PATH=\"$(pwd)/bin:\${PATH}\"|" $HOME/.mdpreviewrc
+sed -ri "s|^MDPREVIEW_PATH=.*$|MDPREVIEW_PATH=\"$(pwd)\"|" $HOME/.mdpreviewrc
 
 echo "Done! Have fun!"
