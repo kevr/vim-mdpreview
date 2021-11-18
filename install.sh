aeff7dd60d47e8d4949e580a28fce16f74e6631f
Kevin Morris <kevr@0cost.org>
2021-11-17 21:00:52 -0800

    fix: remove config file before installing
    This was leaving around old configs before and causing installations
    to fail when reused.
    
    Signed-off-by: Kevin Morris <kevr@0cost.org>

--- install.sh
+++ install.sh
@@ -2,11 +2,12 @@
 # This install script installs the project and dependencies
 # via systemd pip3 to ./lib. It then creates a ~/.mdpreviewrc
 # for the user with paths pointing to this directory.
-set -eou pipefail
+CONFIG="$HOME/.mdpreviewrc"
 
 git pull origin --tags
 
 rm -vf $HOME/.config/systemd/user/mdpreviewd.service
+rm -vf $CONFIG
 
 /usr/bin/pip3 install -I --prefix "$(pwd)/lib" -r requirements.txt .
 
@@ -16,7 +17,7 @@ pypath="$(find "$nvim_path/lib" -type d -name 'site-packages' | xargs)"
 sed -i "s|SCRIPT_PATH|$nvim_path/lib/bin|" \
     $HOME/.config/systemd/user/mdpreviewd.service
 
-cp -vf examples/mdpreviewrc $HOME/.mdpreviewrc
+cp -vf examples/mdpreviewrc $CONFIG
 sed -ri "s|^PYTHONPATH=.*$|PYTHONPATH=\"$pypath:$nvim_path\"|" $HOME/.mdpreviewrc
 sed -ri "s|^PATH=.*$|PATH=\"$nvim_path/lib/bin:$nvim_path/bin:${PATH}|" \
     $HOME/.mdpreviewrc
